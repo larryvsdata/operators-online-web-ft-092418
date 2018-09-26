45d91980abba3489a0ca2b5a764194d7feea77d5
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-26 02:09:11 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,8 @@
 def unsafe?(speed)
   if speed>60 || speed<40:
     return true
+  else 
+    false
   end
 
 end
@@ -8,7 +10,7 @@ end
 
 
 def not_safe?(speed)
-	
+	speed>60? true :speed<40? true:false
 end
 	
 
